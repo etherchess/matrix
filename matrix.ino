563d28478febe05cf779c7fee87cbef1aeaa949d
lschuermann <leon-git@lschuermann.xyz>
2016-06-07 10:39:28 +0200

    Initial commit.

---
+++ matrix.ino
@@ -0,0 +1,16 @@
+#define MATRIX_FIRMVER      0x01
+#define MATRIX_SLAVEADDR    0x01
+
+#include <Wire.h>
+
+uint8_t ledMatrix[8];
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
